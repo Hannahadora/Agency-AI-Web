e9fa040486d331f53bf7c792af2550e3fff452ec
hannah <hannahadora97@gmail.com>
2023-08-22 23:20:21 +0100

    update on AAI section 3 and 4

--- components/shared/BlueLongCard.vue
+++ components/shared/BlueLongCard.vue
@@ -1,13 +1,20 @@
 <template>
   <main class="blc relative">
-    <div class="z-[5] bg-[#F5F7FF] py-[24px] pl-[32px] pr-[58px] flex items-center justify-center rounded-[10px]">
-      <SharedBlueMiniCard  class="mr-[24px]">
-        <img :src="icon" alt="icons" />
-      </SharedBlueMiniCard>
-      <slot></slot>
+
+    <div class="flex items-center">
+        <div class="px-[24px] py-[25px] rounded-[50%] w-[60px] h-[60px] bg-[#E9ECFF] flex flex-col gap-[10px] items-center justify-center mr-[32px]">
+            <p class="text-center text-[#3B57F4] text-[40px] font-[700]">?</p>
+        </div>
+
+       <div class="flex flex-col items-center justify-center">
+            <div class="bg-[#F5F7FF] py-[24px] pl-[32px] pr-[58px] flex items-center justify-center rounded-[10px]">
+                <div class="bg-[#E9ECFF] shadow rounded-[10px] flex items-center justify-center mr-[24px] px-[20px] py-[22px]">
+                    <img :src="icon" alt="icons" />
+                </div>
+                <slot></slot>
+            </div>
+        </div>
     </div>
-    <div class="z-[2] bg-[#3B57F41F] max-w-[609px] mx-auto min-h-[48px] mt-[-40px] rounded-[10px]"></div>
-    <div class="z-[2] bg-[#3B57F41F] max-w-[573px] mx-auto min-h-[58px] mt-[-50px] rounded-[10px]"></div>
   </main>
 </template>
 
@@ -23,7 +30,6 @@ defineProps({
 <style scoped>
 .blc {
     max-width: 678px;
-  /* background: url("/icons/blue-long-card.svg"); */
   background-repeat: no-repeat;
 }
 </style>
