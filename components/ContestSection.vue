dc2fcd8cde24e6f2c29f915a1bc74f7e97c31376
hannah <hannahadora97@gmail.com>
2023-08-24 01:01:00 +0100

    update on UI

--- components/ContestSection.vue
+++ components/ContestSection.vue
@@ -2,28 +2,29 @@
   <main class="relative contest_section py-[100px]">
     <div class="relative d-container py-[36px]">
       <img
-        class="absolute top-[-35px] right-[270px]"
+        class="absolute lg:top-[-35px] top-[-90px] lg:right-[270px] right-[10px]"
         src="/icons/dollar-coin-lg.svg"
         alt="illustration"
       />
       <img
-        class="absolute top-[85px] left-[350px]"
+        class="absolute lg:top-[85px] top-[65px] lg:left-[350px] left-[10px]"
         src="/icons/dollar-coin-md.svg"
         alt="illustration"
       />
-      <h3 class="contest_section-title font-[700] text-[64px] text-center">
+      <h3 class="contest_section-title font-[700] lg:text-[64px] md:text-[50px] text-[40px] text-center">
         The Contest
       </h3>
 
       <div class="flex items-center justify-center mt-[20px] mb-[-40px]">
         <img class="" src="/icons/dollar-coin-sm.svg" alt="illustration" />
       </div>
-      <section class="purple_bg_sect px-[150px] pt-[100px] pb-[150px] rounded-[48px]">
-        <div class="flex flex-col items-center justify-center">
+      <section class="purple_grad_bg rounded-[48px] max-w-[1022px] mx-auto">
+      <section class="w-full purple_bg_sect">
+        <div class=" xl:px-[150px] lg:px-[80px] md:px-[24px] px-[16px] md:pt-[120px] md:pb-[120px] pb-[50px] rounded-[48px] flex flex-col items-center justify-center">
           <img class="" src="/images/cahp-t.png" alt="cash prize text" />
           <img src="/icons/curly-arrow.svg" alt="illustration" />
-          <div class="purple_sect2 px-[56px] py--[16px] mt-[20px] text-center">
-            <p class="leading-[58px] text-[30px]">
+          <div class="purple_sect2 md:px-[56px] px-[30px] md:py-[16px] py-[12px] mt-[20px] text-center">
+            <p class="md:leading-[58px] md:text-[30px] text-[20px]">
               <span class="font-[500] text-[#FFD164]"
                 >72 Hrs Opening Contest: </span
               ><span class="font-[700] text-[#fff]"> 24th - 27th July</span>
@@ -36,52 +37,52 @@
             <div class="flex flex-col items-center justify-center">
               <img src="/icons/second_place.svg" alt="second_place" />
               <div class="mt-[24px] text-center">
-                <p class="text-[24px] font-[500] text-[#fff]">2nd Place</p>
-                <p class="text-[32px] font-[700] text-[#fff]">$750</p>
+                <p class="md:text-[24px] text-[18px] font-[500] text-[#fff]">2nd Place</p>
+                <p class="md:text-[32px] text-[24px] font-[700] text-[#fff]">$750</p>
               </div>
             </div>
             <div class="flex flex-col items-center justify-center">
               <img src="/icons/first_place.svg" alt="first_place" />
               <div class="mt-[24px] text-center">
-                <p class="text-[32px] font-[500] text-[#fff]">1st Place</p>
-                <p class="text-[38px] font-[700] text-[#fff]">$1,500</p>
+                <p class="md:text-[32px] text-[20px] font-[500] text-[#fff]">1st Place</p>
+                <p class="md:text-[38px] text-[28px] font-[700] text-[#fff]">$1,500</p>
               </div>
             </div>
             <div class="flex flex-col items-center justify-center">
               <img src="/icons/third_place.svg" alt="third_place" />
               <div class="mt-[24px] text-center">
-                <p class="text-[24px] font-[500] text-[#fff]">3rd Place</p>
-                <p class="text-[32px] font-[700] text-[#fff]">$500</p>
+                <p class="md:text-[24px] text-[18px] font-[500] text-[#fff]">3rd Place</p>
+                <p class="md:text-[32px] text-[24px] font-[700] text-[#fff]">$500</p>
               </div>
             </div>
           </div>
-          <div class="mt-[60px] w-full grid grid-cols-2 items-end justify-center">
+          <div class="md:mt-[60px] mt-[40px] w-full grid grid-cols-2 items-end justify-center">
             <div class="flex flex-col items-center justify-center">
-              <img src="/icons/third_place.svg" alt="third_place" />
+              <img src="/icons/fourth_place.svg" alt="third_place" />
               <div class="mt-[24px] text-center">
-                <p class="text-[24px] font-[500] text-[#fff]">4th Place</p>
-                <p class="text-[32px] font-[700] text-[#fff]">$100</p>
+                <p class="md:text-[24px] text-[18px] font-[500] text-[#fff]">4th Place</p>
+                <p class="md:text-[32px] text-[24px] font-[700] text-[#fff]">$1,500</p>
               </div>
             </div>
             <div class="flex flex-col items-center justify-center">
-              <img src="/icons/third_place.svg" alt="third_place" />
+              <img src="/icons/fifth_place.svg" alt="third_place" />
               <div class="mt-[24px] text-center">
-                <p class="text-[24px] font-[500] text-[#fff]">5th Place</p>
-                <p class="text-[32px] font-[700] text-[#fff]">$50</p>
+                <p class="md:text-[24px] text-[18px] font-[500] text-[#fff]">5th Place</p>
+                <p class="md:text-[32px] text-[24px] font-[700] text-[#fff]">$500</p>
               </div>
             </div>
           </div>
 
           <div
-            class="mt-[80px] px-[36px] py-[26px] rounded-[8px] bg-[#8B20B9] flex items-center"
+            class="md:mt-[60px] mt-[40px] md:px-[36px] px-[24px] md:py-[26px] py-[18px] rounded-[8px] bg-[#8B20B9] flex items-center"
           >
             <img
               src="/icons/yellow_warning.svg"
               alt="illus"
-              class="mr-[24px]"
+              class="sm:mr-[24px] mr-[20px]"
             />
             <div>
-              <p class="text-[20px] font-[700] text-[#FFD164]">
+              <p class="md:text-[20px] text-[18px] font-[700] text-[#FFD164]">
                 TEAMS OF 2 ALLOWED
               </p>
               <p class="text-[16px] text-[#fff] font-[400]">
@@ -91,6 +92,40 @@
           </div>
         </div>
       </section>
+    </section>
+
+    <section class="rounded-[48px] mt-[40px] max-w-[1022px] mx-auto">
+        <section class="blue_bg_sect md:pt-[90px] pt-[50px] md:pb-[90px] pb-[50px]">
+            <div class="lg:w-[60%] w-[90%] mx-auto flex flex-col items-center justify-center">
+                <div class="flex items-center justify-center">
+                    <img src="/icons/sharp-shutter-speed.svg" alt="shutter"/>
+                    <p class="md:text-[48px] text-[30px] font-[700] text-[#fff]">48 HOURS</p>
+                </div>
+                <p class="font-[800] md:text-[47px] text-[30px] italic text-[#FFDC8A]">Closing Contest</p>
+
+                <div class="blue_sect2 lg:px-[56px] md:px-[36px] px-[24px] md:py-[16px] py-[12px] md:mt-[46px] mt-[30px] text-center">
+                    <p class="md:text-[30px] text-[20px] font-[500] text-[#fff]">Date: <span class="text-[#FFDC8A] font-[700]">27th - 29th July 2023</span></p>
+                </div>
+
+                <div class="md:mt-[60px] mt-[30px] w-full grid grid-cols-2 items-end justify-center">
+            <div class="flex flex-col items-center justify-center">
+              <img src="/icons/first_place_sm.svg" alt="third_place" />
+              <div class="mt-[24px] text-center">
+                <p class="md:text-[24px] text-[18px] font-[500] text-[#fff]">1st Place</p>
+                <p class="md:text-[32px] text-[24px] font-[700] text-[#fff]">$1,500</p>
+              </div>
+            </div>
+            <div class="flex flex-col items-center justify-center">
+              <img src="/icons/second_place.svg" alt="third_place" />
+              <div class="mt-[24px] text-center">
+                <p class="md:text-[24px] text-[18px] font-[500] text-[#fff]">2nd Place</p>
+                <p class="md:text-[32px] text-[24px] font-[700] text-[#fff]">$500</p>
+              </div>
+            </div>
+          </div>
+            </div>
+        </section>
+    </section>
     </div>
       <div class="absolute bottom-[-80px] left-[50%]">
         <img src="/icons/curly-spring-purplebg.svg" alt="curly-spring" />
@@ -105,6 +140,8 @@
   width: 100%;
   background: url("/icons/contest_section-bg.svg");
   background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
 }
 
 .contest_section-title {
@@ -115,17 +152,37 @@
   line-height: 137.687%; /* 88.12px */
 }
 
+.purple_grad_bg {
+  background-color: linear-gradient(178deg, #A11DDC 0%, #5F1A7D 100%),
+}
+
 .purple_bg_sect {
   width: 100%;
+  max-height: 1600px;;
   height: 100%;
   background: url("/icons/contest-section-purplebg.svg");
   background-repeat: no-repeat;
   background-position: center;
   border-radius: 48px;
 }
+.blue_bg_sect {
+    /* width: 100%; */
+  max-height: 900px;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  border-radius: 48px;
+  background-image: linear-gradient(to right, #3B57F4, #0C219798),
+                    url('/icons/contest-section-bluebg.svg');
+}
 .purple_sect2 {
   border-radius: 100px;
   background: #8a00c7;
   box-shadow: 0px 61px 80px 0px rgba(101, 27, 134, 0.35);
+}
+.blue_sect2 {
+    border-radius: 100px;
+background: #263BAB;
+box-shadow: 0px 61px 80px 0px rgba(37, 55, 153, 0.35);
 }
 </style>
---
+++ components/shared/FunnelCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    :class="`bg-[${bgColor}] h-full max-h-[509px] max-w-[392px] w-[100%] relative px-[10px] md:py-[57px] py-[30px] rounded-[24px] border border-[#D7D7D7] overflow-hidden`"
+  >
+    <div class="flex flex-col items-center justify-center">
+        <SvgFunnelDiamond :diamondFill="bgColor" />
+        <h5 class="md:text-[34px] text-[24px] font-[700] md:mt-[32px] mt-[24px] text-[#fff]">AgencyAI</h5>
+        <div class="bg-[#fff] text-center py-[22px] px-[39px] md:mt-[57px] mt-[30px]">
+            <p :class="`text-[${bgColor}] md:text-[24px] text-[20px] font-[600] uppercase`">{{title}}</p>
+        </div>
+        <p class="md:mt-[38px] mt-[30px] font-[600] text-[60px] text-[#fff] text-center">${{ price }}</p>
+    </div>
+
+    <SvgFunnelCurve :curveFill="curveFill" class="absolute bottom-[-10px] left-0" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    curveFill: {
+        type: String,
+        default: '#fff'
+    },
+    bgColor: {
+        type: String,
+        default: '#000'
+    },
+    title: {
+        type: String,
+        default: 'Title'
+    },
+    price: {
+        type: String,
+        default: 'Price'
+    }
+})
+</script>
+
+<style></style>
